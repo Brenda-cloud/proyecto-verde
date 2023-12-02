98799374d83657592675ea7b1df971a7b6021f92
as7haro7 <erickbreack72@gmail.com>
2023-12-02 11:00:22 -0400

    estructura de proyecto mas supabase

---
+++ src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Brenda B.',
+  email: 'brenda@correo.com',
+  photoURL: '/assets/images/avatars/avatar_20.jpg',
+};
