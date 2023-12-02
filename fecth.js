36b5d95c9a3a8d160f820a7de7bae6de5a99b293
as7haro7 <erickbreack72@gmail.com>
2023-12-01 23:59:15 -0400

    conexion reparada y chart

--- fecth.js
+++ fecth.js
@@ -4,46 +4,26 @@ const SUPABASE_ANON_KEY =
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// async function obtenerDatos() {
-//   try {
-//     const { data, error } = await _supabase.from("datos_recolectados").select();
-//     let temperatura = "";
-//     let humedad = "";
-//     data.forEach(function (item) {
-//       temperatura = item.temperatura;
-//       humedad = item.humedad;
-//       // fecha = item.
-//       // console.log(data)
-//     });
-
-//     document.getElementById("temp").innerHTML = temperatura;
-//     document.getElementById("hum").innerHTML = humedad;
-//   } catch (err) {
-//     console.error("Error al recibir los datos de la db", err);
-//   }
-//   // console.log(data);
-// }
 async function obtenerDatos() {
   try {
-      const { data, error } = await _supabase
-          .from('datos_recolectados')
-          .select()
-      let temperatura = ''
-      let humedad = ''
-      data.forEach(function (item) {
-          temperatura = item.temperatura
-          humedad = item.humedad
-          console.log(data)
-      })
-     
-      document.getElementById('temp').innerHTML = temperatura.toFixed(2)
-      document.getElementById('hum').innerHTML = humedad
+    const { data, error } = await _supabase.from("datos_recolectados").select();
+    let temperatura = "";
+    let humedad = "";
+    data.forEach(function (item) {
+      temperatura = item.temperatura;
+      humedad = item.humedad;
+      // fecha = item.
+      // console.log(data)
+    });
+
+    document.getElementById("temp").innerHTML = temperatura.toFixed(2);
+    document.getElementById("hum").innerHTML = humedad;
+  } catch (err) {
+    console.error("Error al recibir los datos de la db", err);
   }
-  catch (err) {
-      console.error("Error al recibir los datos de la db", err);
-  }
-  console.log(data);
-};
+  // console.log(data);
+}
+
 async function subscribeSensor() {
 
   const channel = _supabase
@@ -127,5 +107,45 @@ obtenerComposteras();
 
 
 
+//graficos
 
+new Chart(document.getElementById("line-chart"), {
+    type: 'line',
+    data: {
+      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+      datasets: [{ 
+          data: [86,114,106,106,107,111,133,221,783,2478],
+          label: "Africa",
+          borderColor: "#3e95cd",
+          fill: false
+        }, { 
+          data: [282,350,411,502,635,809,947,1402,3700,5267],
+          label: "Asia",
+          borderColor: "#8e5ea2",
+          fill: false
+        }, { 
+          data: [168,170,178,190,203,276,408,547,675,734],
+          label: "Europe",
+          borderColor: "#3cba9f",
+          fill: false
+        }, { 
+          data: [40,20,10,16,24,38,74,167,508,784],
+          label: "Latin America",
+          borderColor: "#e8c3b9",
+          fill: false
+        }, { 
+          data: [6,3,2,2,7,26,82,172,312,433],
+          label: "North America",
+          borderColor: "#c45850",
+          fill: false
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'World population per region (in millions)'
+      }
+    }
+  });
   
